3c10bc8875692927c012ce5c87fde5e6b7bc2877
OnildoNeto <35998429+OnildoNeto@users.noreply.github.com>
2019-04-11 09:47:11 -0300

    Delete Questão02.py

--- Questão02.py
+++
@@ -1,8 +0,0 @@
-Vp = list()
-Vn = list()
-for i in range(6):
-    valor = int(input())
-    if (valor < 0): ln.append(valor)
-    else:lv.append(valor)
-print("Valor positivo: ",Vp )
-print("Valor negativo: ",Vn)
